774234c2d05e388a5391174031dd05cdf1d90996
Ryan Hawkins <ryan.hawkins@instructure.com>
2023-05-08 09:25:39 -0600

    Add additional autocmds, keymaps, plugins
    There's an autocmd for automagically showing the diagnostic on a line, a
    changed keymap to use fd for finding files instead of regular ol
    Telescope, and a keymap for searching for help.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,21 +6,6 @@
 local lsp_group = vim.api.nvim_create_augroup("LspGroup", { clear = true })
 local easy_quit_group = vim.api.nvim_create_augroup("EasyQuitGroup", { clear = true })
 vim.api.nvim_clear_autocmds({ group = lsp_group, buffer = bufnr })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = lsp_group,
-  buffer = bufnr,
-  callback = function()
-    vim.lsp.buf.format({ bufnr = bufnr })
-    vim.lsp.buf.code_action({
-      apply = true,
-      context = {
-        only = {
-          "source.organizeImports",
-        },
-      },
-    })
-  end,
-})
 
 vim.api.nvim_clear_autocmds({ group = easy_quit_group, buffer = bufnr })
 vim.api.nvim_create_autocmd("FileType", {
@@ -29,10 +14,41 @@ vim.api.nvim_create_autocmd("FileType", {
     "man",
     "help",
     "log",
+    "git",
   },
   buffer = bufnr,
   callback = function()
     vim.api.nvim_buf_set_keymap(0, "i", "<ESC>", "<ESC><cmd>q!<cr>", { noremap = true })
     vim.api.nvim_buf_set_keymap(0, "n", "<ESC>", "<ESC><cmd>q!<cr>", { noremap = true })
   end,
+})
+
+-- Function to check if a floating dialog exists and if not
+-- then check for diagnostics under the cursor
+function OpenDiagnosticIfNoFloat()
+  for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_get_config(winid).zindex then
+      return
+    end
+  end
+  -- THIS IS FOR BUILTIN LSP
+  vim.diagnostic.open_float({
+    scope = "cursor",
+    focusable = false,
+    close_events = {
+      "CursorMoved",
+      "CursorMovedI",
+      "BufHidden",
+      "InsertCharPre",
+      "WinLeave",
+    },
+  })
+end
+
+-- Show diagnostics under the cursor when holding position
+vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  pattern = "*",
+  command = "lua OpenDiagnosticIfNoFloat()",
+  group = "lsp_diagnostics_hold",
 })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local wk = require("which-key")
+
 vim.api.nvim_set_keymap("t", "<esc>", "<C-\\><C-n><cmd>q<cr>", { noremap = true })
+
+wk.register({
+  f = {
+    name = "Find",
+    f = { "<cmd>Telescope fd<cr>", "Find Files" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+  },
+}, {
+  prefix = "<leader>",
+  nnoremap = true,
+})
